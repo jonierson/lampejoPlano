b49a9f8fa900208dde5a2a138ba42e7bd91e393e
jonierson <122365951+jonierson@users.noreply.github.com>
2025-08-04 10:15:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 from groq import Groq
 
 # Tenta carregar a API_KEY a partir do secrets
-api_key = "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"
+api_key = "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"
 
 if not api_key:
     st.error("⚠️ API_KEY não carregada. Verifique se ela está configurada no secrets do Streamlit Cloud.")
